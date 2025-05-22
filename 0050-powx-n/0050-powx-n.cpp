090f8d989b006feb0b7f349cddf90b08a2806d89
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2025-05-22 08:59:37 +0530

    Time: 0 ms (100%), Space: 8.7 MB (14.7%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-     double power(double x,long n)//long n = TLE 
-    {
-        if(n==0) return 1.0;
-        if(n%2 ==0){
-            return  power(x*x,n/2);
+    double func(double x , long n){
+        if(n<=0){
+            return 1.0;
         }
-        return x * power(x,n-1);
+        if(n%2==0){
+            return func(x*x,n/2);
+        }
+        return x*func(x,n-1);
     }
-
 
     double myPow(double x, int n) {
         long num=n;
         if(num<0){
-            return (1.0/power(x,-1 * num));
+            return (1.0/func(x,-1*num));
         }
-        return power(x,num);
+        return func(x,num);
     }
 };
