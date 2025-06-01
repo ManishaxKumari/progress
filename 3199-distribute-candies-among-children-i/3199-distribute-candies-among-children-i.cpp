f3290dc4acd59d745c6a5ce2f6e289c23aaf1ca6
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2025-06-01 22:44:31 +0530

    Time: 41 ms (5.11%), Space: 8.6 MB (26.99%) - LeetHub

--- 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.cpp
+++ 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
-    int distributeCandies(int n, int limit) {
-        int ans=0;
-        for(int i=0;i<=limit;i++){
-            for(int j=0;j<=limit;j++){
-                for(int k=0;k<=limit;k++){
-                    if(i+j+k == n){
-                        ans++;
-                    }
-                }
+    int ans;
+    void func(int n,int limit,int child){
+        if(child==3){
+            if(n==0){
+                ans++;
             }
-
+            return;
         }
+        for(int i=0;i<=limit;i++){
+            int give=i;
+            func(n-give,limit,child+1);
+        }
+    }
+
+    int distributeCandies(int n, int limit) {
+        ans=0;
+        func(n,limit,0);
         return ans;
     }
 };
