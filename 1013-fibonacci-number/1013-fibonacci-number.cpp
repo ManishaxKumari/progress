1656cbdf22e366023eee83f414c6405d915fabaf
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2025-06-17 15:14:56 +0530

    Time: 0 ms (100%), Space: 7.7 MB (64.49%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
 //solve using tabulation (DP)
-    int fib(int n) {
-        vector<int> dp(n+1);
+    // int fib(int n) {
+    //     vector<int> dp(n+1);
+    //     if(n<=1){
+    //         return n;
+    //     }
+    //     dp[0]=0;
+    //     dp[1]=1;
+    //     for(int i=2;i<=n;i++){
+    //         dp[i]=dp[i-1]+dp[i-2];
+    //     }
+    //     return dp[n];
+    // }
+
+
+// space optimization solve withour dp itetrative
+     int fib(int n){
         if(n<=1){
             return n;
         }
-        dp[0]=0;
-        dp[1]=1;
+        int prev2=0;
+        int prev=1;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            int curr=prev2+prev;
+            prev2=prev;
+            prev=curr;
         }
-        return dp[n];
-    }
+        return prev;
+     }
 };
