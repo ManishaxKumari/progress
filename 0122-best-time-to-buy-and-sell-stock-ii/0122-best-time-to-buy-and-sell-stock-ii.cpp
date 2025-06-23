c5441f896000e586fa06e057d2c00e3790362e98
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2025-06-23 15:29:53 +0530

    Time: 6 ms (23.55%), Space: 19.8 MB (21.62%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-      int func(int ind,int buy,vector<int> &arr,int n,vector<vector<int>> &dp){
-        //base case
-        if(ind==n){
-            return 0;
-        }
-        if (dp[ind][buy] != -1) {
-            return dp[ind][buy];
-        }
-        int profit=0;
-        //we can buy stock
-        if(buy==0){ // we hadn't buy anthing prev
-            int buyIt=(-1)*arr[ind] + func(ind+1,1,arr,n,dp);
-            int notBuyIt=func(ind+1,0,arr,n,dp);
-            profit=max(buyIt,notBuyIt);
-        }
-        //we can sell 
-        if(buy==1){ // we had buy previously
-            int sell=arr[ind] + func(ind+1,0,arr,n,dp);
-            int notSell=func(ind+1,1,arr,n,dp);
-            profit=max(sell,notSell);
-        }
-        return dp[ind][buy]=profit;
-
-    }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
         if(n==0) return 0;
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        int ans=func(0,0,prices,n,dp);
-        return ans;
+        int profit=0;
+        vector<vector<int>> dp(n+1,vector<int>(2,-1));
+        dp[n][0] = dp[n][1] = 0;
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<= 1;buy++) {
+                // We can buy the stock
+                if(buy==0){ 
+                    profit=max(0+dp[ind+1][0], (-1)*prices[ind]+dp[ind+1][1]);
+                }
+                // We can sell the stock
+                if (buy==1) { 
+                    profit=max(0+dp[ind+1][1],prices[ind]+dp[ind+1][0]);
+                }
+                dp[ind][buy] = profit;
+            }
+        }
+
+        return dp[0][0];
+        
     }
 };
