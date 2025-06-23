9c13374339506cae64c8193bbb450b22c93677c3
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2025-06-23 17:57:05 +0530

    Time: 124 ms (5.95%), Space: 100.4 MB (11.71%) - LeetHub

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int func(int ind,int buy,vector<int> &arr,int n,vector<vector<int>> &dp,int fee){
+        //base case
+        if(ind==n){
+            return 0;
+        }
+        if (dp[ind][buy] != -1) {
+            return dp[ind][buy];
+        }
+        int profit=0;
+        //we can buy stock
+        if(buy==0){ // we hadn't buy anthing prev
+            int buyIt=(-1)*arr[ind] + func(ind+1,1,arr,n,dp,fee);
+            int notBuyIt=func(ind+1,0,arr,n,dp,fee);
+            profit=max(buyIt,notBuyIt);
+        }
+        //we can sell 
+        if(buy==1){ // we had buy previously
+            int sell=arr[ind]-fee + func(ind+1,0,arr,n,dp,fee);
+            int notSell=func(ind+1,1,arr,n,dp,fee);
+            profit=max(sell,notSell);
+        }
+        return dp[ind][buy]=profit;
+
+    }
+    int maxProfit(vector<int>& prices, int fee) {
+        int n=prices.size();
+        if(n==0) return 0;
+        vector<vector<int>> dp(n,vector<int>(2,-1));
+        int ans=func(0,0,prices,n,dp,fee);
+        return ans;
+    }
+};
