0517bae59faa6ad75d5420748f7f9c689da73782
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2025-06-23 15:57:47 +0530

    Time: 0 ms (100%), Space: 17 MB (86.45%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        if(n==0) return 0;
-        int profit=0;
-        vector<vector<int>> dp(n+1,vector<int>(2,-1));
-        dp[n][0] = dp[n][1] = 0;
-        for(int ind=n-1;ind>=0;ind--){
-            for(int buy=0;buy<= 1;buy++) {
-                // We can buy the stock
-                if(buy==0){ 
-                    profit=max(0+dp[ind+1][0], (-1)*prices[ind]+dp[ind+1][1]);
-                }
-                // We can sell the stock
-                if (buy==1) { 
-                    profit=max(0+dp[ind+1][1],prices[ind]+dp[ind+1][0]);
-                }
-                dp[ind][buy] = profit;
-            }
-        }
+        int n=prices.size(); // Kitne din ke prices diye gaye hain
+        int maxp=0;// Yeh profit store karne ke liye variable hai
 
-        return dp[0][0];
+        //day 1 se leker end tak jana h 
+        for(int i=1;i<n;i++){
+            //agar aj ka price prev day se jyada h -> profit hoga
+            if(prices[i]>prices[i-1]){
+                maxp+=prices[i]-prices[i-1];
+            }
+            //agar aj ka price prev day ke price se kam h to profit nhi hoga kuch mat kro
+            
+        }
+        return maxp;
+        //Jahan bhi price upar gaya, wahan kal kharidke aaj bechne ka socho.
         
+
     }
 };
