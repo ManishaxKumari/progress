7196290b70ab3603626e7b7e2745bbfb956938da
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2025-06-23 15:14:06 +0530

    Time: 8 ms (11.21%), Space: 22.7 MB (6.75%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
+      int func(int ind,int buy,vector<int> &arr,int n,vector<vector<int>> &dp){
+        //base case
+        if(ind==n){
+            return 0;
+        }
+        if (dp[ind][buy] != -1) {
+            return dp[ind][buy];
+        }
+        int profit=0;
+        //we can buy stock
+        if(buy==0){ // we hadn't buy anthing prev
+            int buyIt=(-1)*arr[ind] + func(ind+1,1,arr,n,dp);
+            int notBuyIt=func(ind+1,0,arr,n,dp);
+            profit=max(buyIt,notBuyIt);
+        }
+        //we can sell 
+        if(buy==1){ // we had buy previously
+            int sell=arr[ind] + func(ind+1,0,arr,n,dp);
+            int notSell=func(ind+1,1,arr,n,dp);
+            profit=max(sell,notSell);
+        }
+        return dp[ind][buy]=profit;
+
+    }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        int buy=prices[0];
-        int maxp=0;
-        for(int i=1;i<n;i++){
-            if(prices[i]<buy){
-                buy=prices[i];
-            }
-            else{
-                maxp=maxp+(prices[i]-buy);
-                buy=prices[i];
-            }
-        }
-        return maxp;
+        if(n==0) return 0;
+        vector<vector<int>> dp(n,vector<int>(2,-1));
+        int ans=func(0,0,prices,n,dp);
+        return ans;
     }
 };
