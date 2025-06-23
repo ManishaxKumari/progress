3e04a69463211eeb759f1b1e539e8cf28a60f920
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2025-06-23 17:50:07 +0530

    Time: 15 ms (32.76%), Space: 18.4 MB (24.17%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int func(vector<int>& arr,int n,int ind,int buy,int trans,vector<vector<vector<int>>> &dp){
+        //base case
+        if(ind==n || trans==0){
+            return 0;
+        }
+        int maxp=0;
+        //buy nhi kiya abhi tak ab karo
+        if(dp[ind][buy][trans] !=-1){
+            return dp[ind][buy][trans];
+        }
+        if(buy==0){
+            int buyIt=(-1)*arr[ind] + func(arr,n,ind+1,1,trans,dp);
+            int NotBuy=func(arr,n,ind+1,0,trans,dp);
+            maxp=max(buyIt,NotBuy);
+        }
+        // buy kar liya ab sell karo
+        if(buy==1){
+            int sell=arr[ind] + func(arr,n,ind+1,0,trans-1,dp);
+            int Notsell=func(arr,n,ind+1,1,trans,dp);
+            maxp=max(sell,Notsell);
+        }
+        return dp[ind][buy][trans]=maxp;
+
+    }
+    int maxProfit(int k, vector<int>& arr) {
+         int n=arr.size();
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
+        if(n==0) return 0;
+        int ans=func(arr,n,0,0,k,dp);
+        return ans;
+    }
+};
