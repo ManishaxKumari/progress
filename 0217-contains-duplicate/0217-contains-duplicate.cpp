df677b6f1b5d91eceffec60765cf184b2936940e
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2025-07-05 18:41:21 +0530

    Time: 79 ms (15.93%), Space: 92.8 MB (20.03%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> seen;
-        for(auto it : nums){
-            if(seen.count(it)){
-                return true;
-            }
-            seen.insert(it);
-        }
-        return false;
+        // unordered_set<int> seen;
+        // for(auto it : nums){
+        //     if(seen.count(it)){
+        //         return true;
+        //     }
+        //     seen.insert(it);
+        // }
+        // return false;
+
+        return unordered_set<int>(nums.begin(),nums.end()).size()<nums.size();
     }
 };
