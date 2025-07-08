853e8dd40839fbd414fb27a99df4c69cbd150da2
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2025-07-08 17:43:32 +0530

    Time: 0 ms (100%), Space: 23.6 MB (78.44%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,47 +1,48 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n = nums.size();
-    
-    // Extended Moore's Voting Algorithm
-    int cnt1 = 0, cnt2 = 0;
-    int el1 = INT_MIN, el2 = INT_MIN;
-    
-    for (int i = 0; i < n; i++) {
-        if (cnt1 == 0 && el2 != nums[i]) {
-            cnt1 = 1;
-            el1 = nums[i];
-        } 
-        else if (cnt2 == 0 && el1 != nums[i]) {
-            cnt2 = 1;
-            el2 = nums[i];
-        } 
-        else if (nums[i] == el1) {
-            cnt1++;
-        } 
-        else if (nums[i] == el2) {
-            cnt2++;
-        } 
-        else {
-            cnt1--;
-            cnt2--;
+        int n=nums.size();
+        int maj1=INT_MIN;
+        int maj2=INT_MIN;
+        int cnt1=0,cnt2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]== maj1){
+                cnt1++;
+            }
+            else if(nums[i]==maj2){
+                cnt2++;
+            }
+            else if(cnt1==0){
+                maj1=nums[i];
+                cnt1=1;
+            }
+            else if(cnt2==0){
+                maj2=nums[i];
+                cnt2=1;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
         }
-    }
+        //verification step
+        int freq1=0;
+        int freq2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==maj1) freq1++;
+            if(nums[i]==maj2) freq2++;
+        }
+        vector<int>ans;
+        int mini=n/3+1;
+        if(freq1 >= mini){
+            ans.push_back(maj1);
+        } 
+        if(freq2>=mini && maj1 != maj2){
+            ans.push_back(maj2);
+        }
+        return ans;
+        
 
-    // Second pass to verify actual occurrences
-    cnt1 = 0, cnt2 = 0;
-    for (int i = 0; i < n; i++) {
-        if (nums[i] == el1) cnt1++;
-        if (nums[i] == el2) cnt2++;
-    }
 
-    int limit = n / 3;
-    vector<int> ans;
-    if (cnt1 > limit) ans.push_back(el1);
-    if (cnt2 > limit && el1 != el2) ans.push_back(el2);  // Avoid duplicates
-
-    return ans;
-
-     
     }
 };
