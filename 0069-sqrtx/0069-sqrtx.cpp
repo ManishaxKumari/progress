37bb4904c3c1cf9a37768b79ab73b8c04f4f6eb6
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2025-10-02 10:49:45 +0530

    Time: 0 ms (100%), Space: 8.7 MB (14.78%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    bool ispossible( long long mid,int x){
-        long long num=mid*mid;
-        if(num<=x){
-            return true;
-        }
-        return false;
-    }
     int mySqrt(int x) {
         long long low=1;
         long long high=x;
         int ans=0;
         while(low<=high){
-            long long mid=(low+high)/2;
-            if(ispossible(mid,x)){
+            long long mid=low+(high-low)/2;
+            long long res=mid*mid;
+            if(res<=x){
                 ans=mid;
-                low=mid+1; // ye ans possible h but or higher sidr jaker dekho
+                low=mid+1;
             }
             else{
-                high=mid-1; // left side jao bhut bada ho gya mid
+                high=mid-1;
             }
+        
         }
         return ans;
     }
-    
 };
