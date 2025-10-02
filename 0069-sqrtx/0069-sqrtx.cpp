56c8bc7cf0efe6f0880af430c90930c691341064
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2025-10-02 10:50:33 +0530

    Time: 0 ms (100%), Space: 8.5 MB (84.99%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -3,12 +3,12 @@ public:
     int mySqrt(int x) {
         long long low=1;
         long long high=x;
-        int ans=0;
+        //int ans=0;
         while(low<=high){
             long long mid=low+(high-low)/2;
             long long res=mid*mid;
             if(res<=x){
-                ans=mid;
+                //ans=mid;
                 low=mid+1;
             }
             else{
@@ -16,6 +16,7 @@ public:
             }
         
         }
-        return ans;
+        //return ans;
+        return high;
     }
 };
