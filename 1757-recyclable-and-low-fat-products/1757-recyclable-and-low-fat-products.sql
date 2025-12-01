c0648e0c3ee20d0c4e7618276a5f0a584c342753
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2025-12-01 15:42:21 +0530

    Time: 507 ms (47.37%), Space: 0B (100%) - LeetHub

---
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+#ENUM is a datatype in SQL that lets you store one value from a list of predefined values.
+SELECT product_id FROM Products WHERE low_fats='Y' AND recyclable='Y';
