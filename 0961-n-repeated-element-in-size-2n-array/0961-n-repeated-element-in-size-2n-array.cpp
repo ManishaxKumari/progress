a518ae086f1609a55f806d1698a5d371d98a0007
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-01-03 02:23:49 +0530

    Time: 0 ms (100%), Space: 28.6 MB (53.81%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -5,12 +5,13 @@ public:
         int n=nums.size();
         for(int i=0;i<n;i++){
             mpp[nums[i]]++;
+            if(mpp[nums[i]]>1) return nums[i];
         }
-        for(auto &i : mpp){
-            if(i.second==n/2){
-                return i.first;
-            }
-        }
+        // for(auto &i : mpp){
+        //     if(i.second==n/2){
+        //         return i.first;
+        //     }
+        // }
         return -1;
     }
 };
