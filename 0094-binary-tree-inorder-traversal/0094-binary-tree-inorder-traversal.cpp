f7dd14e6682d5ea1910ef461af61b2bb573e4eb9
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-01-27 15:04:49 +0530

    Time: 0 ms (100%), Space: 10.7 MB (87.94%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,30 +11,15 @@
  */
 class Solution {
 public:
-//morris traversal!!
+    void inorder(TreeNode* root,vector<int>&arr){
+        if(root==nullptr) return;
+        inorder(root->left,arr);
+        arr.push_back(root->val);
+        inorder(root->right,arr);
+    }
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> res; // to store 
-        TreeNode* curr=root; 
-        while(curr!=nullptr){
-            if(curr->left == nullptr){ //agar left khatam ho jata h tab hi usko store karte h 
-                res.push_back(curr->val); // yaha per store kar lo !
-                curr=curr->right; // uske right mai chl joa
-            }
-            else{
-                TreeNode* leftchild=curr->left; // ky left child hai?? haai null nhi h exist karta h to phir 
-                while(leftchild->right !=nullptr){ // ussi left child ke righmost node mai jao (jab tak vo null ke equal na ho)
-                    leftchild=leftchild->right;
-                }
-                leftchild->right=curr; // jab rightmost mil jayye toh usko curr se connect kar do 
-                //curr->left ko NULL mark kar do (delete) kar sakte ho
-                TreeNode* temp=curr;
-                curr=curr->left;
-                temp->left=NULL;
-                //purana connection thodna hoga kyuki agar curr dubara pahucha root mai toh or left agar
-                //delet nhi hua hoga to dubara left wla call ho jyega !!(ye recursion nhi h isliye)
-            }
-        }
-        return res;
-
+        vector<int>arr;
+        inorder(root,arr);
+        return arr;
     }
 };
