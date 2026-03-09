4a3b70efef3affb285d44e8c33780a61e98fdc3e
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-03-09 20:02:29 +0530

    Time: 17 ms (77.76%), Space: 79.4 MB (96.85%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,42 +1,27 @@
 class Solution {
 public:
-    vector<int>findnse(vector<int>& arr){
-        int n=arr.size();
-        vector<int>ans(n);
-        stack<int>st;
-        for(int i=n-1;i>=0;i--){
-            int currele=arr[i];
-            while(!st.empty() && arr[st.top()]>=arr[i]){
-                st.pop();
-            }
-            ans[i]=!st.empty() ? st.top() : n;
-            st.push(i);
-        }
-        return ans;
-
-    }
-    vector<int> findpse(vector<int>& arr){
-        int n=arr.size();
-        vector<int>ans(n);
-        stack<int>st;
-        for(int i=0;i<n;i++){
-            int currele=arr[i];
-            while(!st.empty() && arr[st.top()]>=arr[i]){
-                st.pop();
-            }
-            ans[i]=!st.empty() ? st.top() : -1;
-            st.push(i);
-        }
-        return ans;
-    }
     int largestRectangleArea(vector<int>& heights) {
         int n=heights.size();
-        vector<int> nse=findnse(heights);
-        vector<int>pse=findpse(heights);
+        stack<int>st;
         int maxarea=0;
-        int area;
+        int area,nse,pse;
         for(int i=0;i<n;i++){
-            area=heights[i]*(nse[i]-pse[i]-1);
+            while(!st.empty() && heights[st.top()]>=heights[i]){
+                nse=i;
+                int ind=st.top();
+                st.pop();
+                pse=st.empty() ? -1 : st.top();
+                area=heights[ind]*(nse-pse-1);
+                maxarea=max(maxarea,area);
+            }
+            st.push(i);
+        }
+        while(!st.empty()){
+            nse=n;
+            int ind=st.top();
+            st.pop();
+            pse=st.empty()?-1 : st.top();
+            area=heights[ind]*(nse-pse-1);
             maxarea=max(maxarea,area);
         }
         return maxarea;
