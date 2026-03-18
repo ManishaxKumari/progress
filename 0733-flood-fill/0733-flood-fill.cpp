db98ed1915d4e860122519a2d89c0dcc0c9022bf
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-03-18 18:50:37 +0530

    Time: 0 ms (100%), Space: 18.2 MB (55.72%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    bool valid(int &i,int &j,int n,int m){
-        if(i<0 || i >=n) return false;
-        if(j<0 || j>=m) return false;
-        return true;
+    int dr[4]={-1,0,1,0};
+    int dc[4]={0,1,0,-1};
+    bool valid(int i,int j,int m,int n){
+        return (i>=0 && j>=0 && i<m && j<n);
     }
-    int delrow[4]={-1,0,1,0};
-    int delcol[4]={0,1,0,-1};
-    void dfs(int row,int col,int color,int inicol,vector<vector<int>> &ans){
-        ans[row][col]=color;
-        int n=ans.size();
-        int m=ans[0].size();
+    void dfs(int r,int c,int inicolor,int color,vector<vector<int>> &ans){
+        int m=ans.size();
+        int n=ans[0].size();
+        ans[r][c]=color;
         for(int i=0;i<4;i++){
-            int nrow=row+delrow[i];
-            int ncol=col+delcol[i];
-            if(valid(nrow,ncol,n,m) && ans[nrow][ncol]==inicol){
-                dfs(nrow,ncol,color,inicol,ans);
+            int nr=r+dr[i];
+            int nc=c+dc[i];
+            if(valid(nr,nc,m,n) && ans[nr][nc]==inicolor){
+                dfs(nr,nc,inicolor,color,ans);
             }
         }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        if(image[sr][sc]==color) return image;
-        int inicol=image[sr][sc];
-        vector<vector<int>> ans=image;
-        dfs(sr,sc,color,inicol,ans);
+       int m=image.size();
+       int n=image[0].size();
+       int inicolor=image[sr][sc];
+       if(inicolor==color) return image;
+       vector<vector<int>> ans=image;
+       dfs(sr,sc,inicolor,color,ans);
         return ans;
+
     }
 };
