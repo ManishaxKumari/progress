b016ab9397e305fdf2fd9e31bcd4bcd971c1c90c
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-03-18 19:51:19 +0530

    Time: 8 ms (61.8%), Space: 36.1 MB (37.83%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int dr[4]={-1,0,1,0};
     int dc[4]={0,1,0,-1};
-    bool valid(int &i,int &j,int m,int n){
+    bool valid(int i,int j,int m,int n){
         return i>=0 && j>=0 && i<m && j<n ;
     }
-    void dfs(int r,int c,vector<vector<int>> &ans ){
+    void dfs(vector<vector<int>> &ans,int r,int c){
+        ans[r][c]=0; //visited
         int m=ans.size();
         int n=ans[0].size();
-        ans[r][c]=0;
         for(int i=0;i<4;i++){
             int nr=r+dr[i];
             int nc=c+dc[i];
             if(valid(nr,nc,m,n) && ans[nr][nc]==1){
-                dfs(nr,nc,ans);
+                dfs(ans,nr,nc);
             }
         }
     }
@@ -21,24 +21,25 @@ public:
         int m=grid.size();
         int n=grid[0].size();
         vector<vector<int>> ans=grid;
-        //remove boundary connected land
         for(int i=0;i<m;i++){
-            if(ans[i][0]==1) dfs(i,0,ans);
-            if(ans[i][n-1]==1) dfs(i,n-1,ans);
+            if(ans[i][0]==1) dfs(ans,i,0);
+            if(ans[i][n-1]==1) dfs(ans,i,n-1);
         }
         for(int j=0;j<n;j++){
-            if(ans[0][j]==1) dfs(0,j,ans);
-            if(ans[m-1][j]==1) dfs(m-1,j,ans);
+            if(ans[0][j]==1) dfs(ans,0,j);
+            if(ans[m-1][j]==1) dfs(ans,m-1,j);
         }
-
-        //cnt remaining land
         int cnt=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(ans[i][j]==1) cnt++;
+                if(ans[i][j]==1){
+                    cnt++;
+                }
             }
         }
         return cnt;
-        
+
+
+
     }
 };
