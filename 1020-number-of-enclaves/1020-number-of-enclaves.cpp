71705887c6fa56ca535fddcf73e825d605511260
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-03-27 11:28:35 +0530

    Time: 9 ms (47.96%), Space: 31.7 MB (78.01%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -5,40 +5,55 @@ public:
     bool valid(int i,int j,int m,int n){
         return i>=0 && j>=0 && i<m && j<n ;
     }
-    void dfs(int r,int c,vector<vector<int>>& grid){
-        int m=grid.size();
-        int n=grid[0].size();
-        grid[r][c]=0;
-        for(int i=0;i<4;i++){
-            int nr=r+dr[i];
-            int nc=r+dc[i];
-            if(valid(nr,nc,m,n) && grid[nr][nc]==1){
-                dfs(nr,nc,grid);
-            }
-        }
-
-    }
     int numEnclaves(vector<vector<int>>& grid) {
         int m=grid.size();
-        int n=grid[0].size(); // for looping we have to find m x n
-        //loop for find all boundary touch lands and start dfs from it 
+        int n=grid[0].size();
+        vector<vector<int>> ans=grid;
+        queue<pair<int,int>> q;
         for(int i=0;i<m;i++){
-            if(grid[i][0] ==1) dfs(i,0,grid);
-            if(grid[i][n-1] ==1) dfs(i,n-1,grid);
+           if(ans[i][0]==1){
+                q.push({i,0});
+                ans[i][0] = 0;
+            }
+            if(ans[i][n-1]==1){
+                q.push({i,n-1});
+                ans[i][n-1]=0;
+            }
         }
         for(int j=0;j<n;j++){
-            if(grid[0][j]==1) dfs(0,j,grid);
-            if(grid[m-1][j]==1) dfs(m-1,j,grid);
+            if(ans[0][j]==1){
+                q.push({0,j});
+                ans[0][j]=0;
+            }
+            if(ans[m-1][j]==1){
+                q.push({m-1,j});
+                ans[m-1][j]=0;
+            }
         }
-        //enclosed land count
+        while(!q.empty()){
+            auto [r,c]=q.front();
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+                if(valid(nr,nc,m,n) && ans[nr][nc]==1){
+                    ans[nr][nc]=0;
+                    q.push({nr,nc});
+                }
+        }
+        }
+
         int cnt=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==1) cnt++;
+                if(ans[i][j]==1){
+                    cnt++;
+                }
             }
         }
         return cnt;
-        
+
+
 
     }
 };
