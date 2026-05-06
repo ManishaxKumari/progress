438710c0309659da45cea88d95e65701c3070756
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-05-06 12:43:41 +0530

    Time: 0 ms (100%), Space: 8.6 MB (16.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,44 +1,14 @@
 class Solution {
 public:
-//memo
-    // int func(int n,vector<int> &dp){
-    //     if(n<0) return 0;
-    //     if(n==0) return 1;
-    //     if(dp[n] !=-1){
-    //         return dp[n];
-    //     }
-    //     return dp[n]=func(n-1,dp) + func(n-2,dp);
-    // }
-    // int climbStairs(int n) {
-    //     vector<int> dp(n+1,-1);
-    //     return func(n,dp);
-    // }
-
-//tabulation
-    int climbStairs(int n){
-        vector<int> dp(n+1);
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+    int ways(int n,vector<int> &dp){
+        if(n==0 || n==1){
+            return 1;
         }
-        return dp[n];
-
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=ways(n-1,dp)+ways(n-2,dp);
     }
-//space optimization
-
-    // int climbStairs(int n) {
-    //     int prev2 = 1;
-    //     int prev = 1;
-    //     for (int i = 2; i <= n; i++) {
-    //         int cur_i = prev2 + prev;
-    //         prev2 = prev;
-    //         prev = cur_i;
-    //     }
-    //     return prev;
-
-    // }
-
-
-
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return ways(n,dp);
+    }
 };
