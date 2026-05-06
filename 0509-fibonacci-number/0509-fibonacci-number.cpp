85e5fa7c74559bd06f81617cf8c14c4b39b70b72
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-05-06 10:16:44 +0530

    Time: 0 ms (100%), Space: 8.2 MB (11.32%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
+    int solve(int n,vector<int> &dp){
+        //base case
+        if(n<=1) return n;
+        if(dp[n]!=-1) return dp[n]; // already precompute - reuse here
+        return dp[n]=solve(n-1,dp) + solve(n-2,dp);
+
+    }
     int fib(int n) {
-        if(n<=1){
-            return n;
-        }
-        return fib(n-1)+fib(n-2);
+        vector<int>dp(n+1,-1); // memorization
+        return solve(n,dp);
     }
 };
