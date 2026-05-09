7619e7a16aeba9e4743089514a6d73a06e55b6b4
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-05-09 19:23:24 +0530

    Time: 13 ms (30.07%), Space: 7.7 MB (65.1%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
-//tabulation
+//recusrion
     int fib(int n) {
-        if(n<=1) return n;
-        vector<int> dp(n+1);
-        dp[0]=0;
-        dp[1]=1;
-        for(int i=2 ;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+       if(n<=1) return n;
+       return fib(n-1)+fib(n-2);
 
     }
 };
