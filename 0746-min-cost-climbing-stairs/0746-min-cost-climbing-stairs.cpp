50ee9766220b333a3d247f3ef7c388d785e95bef
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-05-09 21:13:20 +0530

    Time: 0 ms (100%), Space: 17.7 MB (27.37%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-//recusrion
-    int solve(vector<int>&dp,vector<int>& cost,int n){
-        if(n<=1) return cost[n];
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=cost[n]+min(solve(dp,cost,n-1),solve(dp,cost,n-2));
-    }
+//tabulation
     int minCostClimbingStairs(vector<int>& cost) {
         cost.push_back(0); // because their is no cost to reach top
         int n=cost.size();
-        vector<int> dp(n,-1);
-        solve(dp,cost,n-1);
+        vector<int> dp(n);
+        //base case
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        for(int i=2;i<n;i++){
+            dp[i]=cost[i]+min(dp[i-1],dp[i-2]);
+        }
+
         return dp[n-1];
     }
 };
