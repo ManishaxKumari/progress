9b92719fe54d3a893580810ad2cf9ca265ee0df8
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-05-09 19:56:28 +0530

    Time: 0 ms (100%), Space: 8.6 MB (32.13%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
+//memorzation
+    int solve(int n,vector<int>&dp){
+        if(n==0 || n==1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+    }
     int climbStairs(int n) {
-        int prev2=1;
-        int prev1=1;
-        for(int i=2;i<=n;i++){
-            int curr=prev1+prev2;
-            prev2=prev1;
-            prev1=curr;
-        }
-        return prev1;
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
     }
 };
