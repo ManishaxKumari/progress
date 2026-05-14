2807e86bdf3371f86ca473eb33e9b69f60c70008
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-05-14 19:09:19 +0530

    Time: 10 ms (79.84%), Space: 17.4 MB (64.18%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,12 +10,14 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==nullptr) return nullptr;
+        //BASE CASE
+        if(root==nullptr) return nullptr; 
         if(root==p || root==q) return root;
-        TreeNode* left =lowestCommonAncestor(root->left,p,q);
-        TreeNode* right=lowestCommonAncestor(root->right,p,q);
-        if(left!=nullptr && right!=nullptr) return root;
-        return (left!=nullptr)? left:right;
-
+        //POSTORDER - for decide lca we need left and right answer both 
+        TreeNode* leftside=lowestCommonAncestor(root->left,p,q);
+        TreeNode* rightside=lowestCommonAncestor(root->right,p,q);
+        //PROCESS
+        if(leftside!=nullptr && rightside!=nullptr) return root; // if you found both return root -> lca
+        return (leftside!=nullptr) ? leftside : rightside; // if only found one then return what found
     }
 };
