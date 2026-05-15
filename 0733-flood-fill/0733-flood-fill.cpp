dcaffe7783818ba95b31d13366ce04951959c433
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-05-15 10:28:14 +0530

    Time: 0 ms (100%), Space: 18 MB (74.53%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -2,28 +2,26 @@ class Solution {
 public:
     int dr[4]={-1,0,1,0};
     int dc[4]={0,1,0,-1};
-    bool valid(int i, int j,int m, int n){
+    bool valid(int i,int j,int m,int n){
         return (i>=0 && j>=0 && i<m && j<n);
     }
-    void dfs(int r,int c,int inicolor,int color,vector<vector<int>> &ans){
+    void dfs(vector<vector<int>> &ans,int r,int c,int inicolor,int color){
         int m=ans.size();
         int n=ans[0].size();
         ans[r][c]=color;
         for(int i=0;i<4;i++){
-            int nr=r+dr[i];
-            int nc=c+dc[i];
+            int nr= r+dr[i];
+            int nc= c+dc[i];
             if(valid(nr,nc,m,n) && ans[nr][nc]==inicolor){
-                dfs(nr,nc,inicolor,color,ans);
+                dfs(ans,nr,nc,inicolor,color);
             }
         }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int m=image.size();
-        int n=image[0].size();
         int inicolor=image[sr][sc];
         if(inicolor==color) return image;
         vector<vector<int>> ans=image;
-        dfs(sr,sc,inicolor,color,ans);
+        dfs(ans,sr,sc,inicolor,color);
         return ans;
     }
 };
