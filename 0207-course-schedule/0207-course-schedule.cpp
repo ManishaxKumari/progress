6bdf90ab94b2e947907590528f0e38c7b5841594
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-05-20 10:06:28 +0530

    Time: 9 ms (27.41%), Space: 20 MB (38.61%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,16 +1,18 @@
 class Solution {
+    //if given graph is acyclic - true
+    //if graph is cyclic- false
+    //kahn algorithm for check this
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>>adj(numCourses);
-        vector<int>indegree(numCourses,0);
-        for(auto it : prerequisites){
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses);
+        for(auto it : prerequisites ){
             int a=it[0];
             int b=it[1];
             adj[b].push_back(a); //b->a
-            indegree[a]++; // a prereq increses
+            indegree[a]++;
         }
         queue<int>q;
-
         for(int i=0;i<numCourses;i++){
             if(indegree[i]==0){
                 q.push(i);
@@ -28,6 +30,7 @@ public:
                 }
             }
         }
+
         return cnt==numCourses;
     }
 };
