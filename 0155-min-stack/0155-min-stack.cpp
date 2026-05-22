d1ce073a427ba3ffce5aa5d800cbf85a815ea1a5
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-05-22 05:48:53 +0530

    Time: 0 ms (100%), Space: 23.2 MB (83.7%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,30 +1,50 @@
 class MinStack {
 private:
-    stack<pair<int,int>>st;
+typedef long long ll;
+    stack<ll>st;
+    ll mini; // this store minimum value 
 public:
-    MinStack() {
+    MinStack() { //empty constructor
         
     }
     
     void push(int val) {
+        ll x=val;
         if(st.empty()){
-            st.push({val,val});
-            return;
+            mini=x; // this new value is minimum
+            st.push(x); //push as it is
+            return;//job is done just return
         }
-        int mini=min(getMin(),val);
-        st.push({val,mini});
+        if(x>=mini){ //dont need to do any modification just push it
+            st.push(x);
+        }
+        else{ //we found new minimum 
+             // modify the value; 
+            st.push(2LL*x-mini); // push modify value;
+            mini=x; // change mini into original minimum value;
+        }
     }
     
     void pop() {
+        if(st.empty()) return; //noting to pop out
+        ll x=st.top(); //get the top value;
         st.pop();
+        if(x<mini){
+            mini=2LL*mini-x; // 2*(original val) - (value that pop out)
+        }
     }
     
     int top() {
-        return st.top().first;
+        if(st.empty()){
+            return -1;
+        }
+        ll x=st.top();
+        if(x>=mini) return x;
+        return mini;
     }
     
     int getMin() {
-        return st.top().second;
+        return mini;
     }
 };
 
