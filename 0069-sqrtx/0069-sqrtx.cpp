56fcd256f805cf31da20e1bd3c38d4cec1e2f1d3
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-05-25 08:17:44 +0530

    Time: 0 ms (100%), Space: 8.5 MB (85.48%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    using ll=long long;
+    using ll = long long;
     int mySqrt(int x) {
         ll low=0;
         ll high=x;
@@ -8,14 +8,14 @@ public:
             ll mid=(low+high)/2;
             ll ans=mid*mid;
             ll target=x;
-            if(ans == target) return (int)mid;
-            else if(ans<target){
-                low=mid+1;
+            if(ans==target){
+                return mid;
             }
-            else{
+            else if ( ans > target){
                 high=mid-1;
             }
+            else low=mid+1;
         }
-        return (int)high;
+        return high;
     }
 };
