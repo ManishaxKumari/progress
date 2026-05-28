d63b613ac385541675f2aadf08d98ebfc4f8cda9
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-05-28 10:49:59 +0530

    Time: 0 ms (100%), Space: 26.2 MB (33.35%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,33 +2,33 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n=nums.size();
-        //edge cases
         if(n==1) return nums[0];
-        if(nums[0] != nums[1]) return nums[0];
-        if(nums[n-1] != nums[n-2]) return nums[n-1];
-        //trim search space
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
         int low=1;
         int high=n-2;
         while(low<=high){
             int mid=(low+high)/2;
-            //if mid is single ele
-            if(nums[mid] !=nums[mid-1] && nums[mid] != nums[mid+1]){
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
                 return nums[mid];
             }
-            // We are in the left part:
-            if ((mid % 2 == 1 && nums[mid] == nums[mid - 1])
-                || (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
-                // Eliminate the left half:
-                low = mid + 1;
+            if(mid%2 ==0 ){ //even index
+                if(nums[mid]==nums[mid-1]){ //pair in left
+                    high=mid-1;
+                }
+                else { //pair in right
+                    low=mid+1;
+                }
             }
-            // We are in the right part:
-            else {
-                // Eliminate the right half:
-                high = mid - 1;
+            else{ //odd index
+                if(nums[mid]==nums[mid-1]){ //pair in left
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
             }
         }
         return -1;
-
-
     }
 };
