466f89f7d41d559ae1ad86e6fe9ed3d4325620b5
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-05-28 11:52:39 +0530

    Time: 0 ms (100%), Space: 26.2 MB (33.35%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,33 +2,20 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n=nums.size();
-        if(n==1) return nums[0];
-        if(nums[0]!=nums[1]) return nums[0];
-        if(nums[n-1]!=nums[n-2]) return nums[n-1];
-        int low=1;
-        int high=n-2;
-        while(low<=high){
+        int low=0;
+        int high=n-1;
+        while(low<high){
             int mid=(low+high)/2;
-            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
-                return nums[mid];
+            if(mid%2==1) mid--; // make mid even index
+            if(nums[mid]==nums[mid+1]){ // normal side
+                low=mid+2; // go ahead
             }
-            if(mid%2 ==0 ){ //even index
-                if(nums[mid]==nums[mid-1]){ //pair in left
-                    high=mid-1;
-                }
-                else { //pair in right
-                    low=mid+1;
-                }
+            else { //disruption side
+                high=mid;
             }
-            else{ //odd index
-                if(nums[mid]==nums[mid-1]){ //pair in left
-                    low=mid+1;
-                }
-                else{
-                    high=mid-1;
-                }
-            }
+
         }
-        return -1;
+        return nums[low];
+
     }
 };
