86ad15abd05a2545a72d72b56d6277258ab708c7
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-05-28 08:31:47 +0530

    Time: 7 ms (0.84%), Space: 17.9 MB (66.28%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -6,32 +6,25 @@ public:
         int high=n-1;
         while(low<=high){
             int mid=(low+high)/2;
-            if(nums[mid]==target){
-                return true;
-            }
+            if(nums[mid]==target) return true;
             if(nums[low]==nums[mid] && nums[mid]==nums[high]){
-                low=low+1;
-                high =high-1;
+                low++;
+                high--;
                 continue;
             }
-            //which part is sorted?
-            if (nums[low]<=nums[mid] ){ //left part sorted
-                if( nums[low]<=target && target <=nums[mid]){
-                    high=mid-1;
+            if(nums[low]<=nums[mid]){ // left part sorted
+                if(nums[low]<=target && target <=nums[mid]){ //element present in left sorted part
+                    high=mid-1; // go to the left side
                 }
-                else {
-                    low=mid+1;
-                }
+                else low=mid+1;
             }
-            else{//right part sorted
-                if(nums[mid]<=target && target <=nums[high]){
-                    low=mid+1;
+            else { //right part is sorted
+                if(nums[mid]<=target && target <=nums[high]){ // element present in right part
+                    low=mid+1; //go to the right part;
                 }
-                else{
-                    high=mid-1;
-                }
+                else high=mid-1;
             }
         }
-        return false;   
+        return false;
     }
 };
