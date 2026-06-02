3ce48e32d829d998a2d32f177640662ffcc73578
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-06-02 11:40:50 +0530

    Time: 0 ms (100%), Space: 16.5 MB (31.03%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reversell(ListNode* temp){
+        ListNode* prev=nullptr;
+        ListNode* curr=temp;
+        while(curr!=nullptr){
+            ListNode* next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+    int lenght(ListNode* head){
+        int cnt=0;
+        ListNode* temp=head;
+        while(temp!=nullptr){
+            cnt++;
+            temp=temp->next;
+        }
+        return cnt;
+    }
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head==nullptr) return nullptr;
+        if(head->next==nullptr && k==1){
+            return head;
+        }
+        ListNode* fast=head;
+        k=k%(lenght(head));
+        for(int i=0;i<k;i++){
+            fast=fast->next;
+        }
+        ListNode* slow=head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next;
+        }
+
+        ListNode* prevnode=head;
+        ListNode* nextnode=slow->next;
+        slow->next=nullptr;
+        reversell(head);
+        head=slow;
+        prevnode->next=reversell(nextnode);
+
+        return reversell(head);
+
+    }
+};
