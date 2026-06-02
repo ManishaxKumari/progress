a820d5d90aff1d856ab60e3b5284ba1b84e853d2
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-06-02 14:57:44 +0530

    Time: 81 ms (5.11%), Space: 86.2 MB (5.05%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,22 +10,56 @@
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
-        vector<int>arr;
-        ListNode* temp=head;
-        while(temp!=nullptr){
-            arr.push_back(temp->val);
+    ListNode* mergeTwosortedll(ListNode* list1 , ListNode* list2){
+        ListNode* dummy=new ListNode(-1);
+        ListNode* temp=dummy;
+        while(list1 && list2){
+            if(list1->val >= list2->val){
+                temp->next=list2;
+                list2=list2->next;
+            }
+            else {
+                temp->next=list1;
+                list1=list1->next;
+            }
             temp=temp->next;
         }
-
-        sort(arr.begin(),arr.end());
-        temp=head;
-        int i=0;
-        while(temp!=nullptr){
-            temp->val=arr[i];
-            temp=temp->next;
-            i++;
-        }
+        temp->next=(list1)? list1 : list2;
+        ListNode* head=dummy->next;
+        delete dummy;
         return head;
+    }
+
+    ListNode* getmiddle(ListNode* temp){
+        if(temp==nullptr || temp->next==nullptr){
+            return temp;
+        }
+        ListNode* slow=temp;
+        ListNode* fast=temp->next; // beacuse we need before first middle not second
+
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+
+    ListNode* sortList(ListNode* head) {
+        if(head==nullptr || head->next==nullptr){
+            return head;
+        }
+        ListNode* middle=getmiddle(head); //find mid point
+        //divide into 2 half
+        ListNode* left=head;
+        ListNode* right=middle->next;
+        middle->next=nullptr;
+
+        //recursively sort both side
+        left=sortList(left);
+        right=sortList(right);
+
+        //merge sorted side
+        return mergeTwosortedll(left,right);
+
     }
 };
