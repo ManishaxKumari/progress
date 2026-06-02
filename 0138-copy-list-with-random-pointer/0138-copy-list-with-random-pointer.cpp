223b8ba261b748d4b9404b9de0bb5f7f7df01d90
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-06-02 19:07:49 +0530

    Time: 8 ms (26.91%), Space: 15 MB (85.79%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -16,26 +16,49 @@ public:
 
 class Solution {
 public:
+    void insertCopyInBetween(Node* head){
+        Node* temp=head;
+        while(temp!=nullptr){
+            Node* nextelement=temp->next;
+            Node* copy= new Node(temp->val);
+            copy->next=nextelement;
+            temp->next=copy;
+            temp=nextelement;
+        }
+    }
+    void connectRandomPointers(Node* head){
+        Node* temp=head;
+        while(temp!=nullptr){
+            Node* copynode=temp->next;
+            if(temp->random){
+                copynode->random=temp->random->next;
+            }
+            else{
+                copynode->random=nullptr;
+            }
+            temp=temp->next->next;
+        }
+    }
+
+    Node* getDeepCopyList(Node* head){
+        Node* temp=head;
+        Node* dummynode=new Node(-1);
+        Node* res=dummynode;
+        while(temp!=nullptr){
+            res->next=temp->next;
+            res=res->next;
+
+            temp->next=temp->next->next;
+            temp=temp->next;
+        }
+        return dummynode->next;
+    }
+
+
     Node* copyRandomList(Node* head) {
         if(head==nullptr) return nullptr;
-
-        unordered_map<Node*,Node*> mpp;
-        Node* temp=head;
-
-        while(temp!=nullptr){
-            Node* newnode=new Node(temp->val);
-            mpp[temp]=newnode;
-            temp=temp->next;
-        }
-
-        temp=head;
-
-        while(temp!=nullptr){
-            Node* copynode=mpp[temp];
-            copynode->next=mpp[temp->next];
-            copynode->random=mpp[temp->random];
-            temp=temp->next;
-        }
-        return mpp[head];
+        insertCopyInBetween(head);
+        connectRandomPointers(head);
+        return getDeepCopyList(head);
     }
 };
