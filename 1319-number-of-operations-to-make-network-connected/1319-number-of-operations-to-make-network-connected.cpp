8ae536e79380ce28330896db9861b3fbd5cf4995
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-06-14 06:40:52 +0530

    Time: 24 ms (37.56%), Space: 47.3 MB (32.82%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,70 +1,70 @@
-class DisjointSet {
+class disjoinSet{
 public:
-    vector<int>rank,parent,size;
-    DisjointSet(int n) {
+    vector<int>rank,size,parent;
+    disjoinSet(int n){
         rank.resize(n+1,0);
-        parent.resize(n+1);
         size.resize(n+1,1);
-        for(int i=0;i<=n;i++){ 
+        parent.resize(n+1); //mistake
+        for(int i=0;i<=n;i++){
             parent[i]=i;
         }
     }
-    int findHighestParent(int node){
-        if(parent[node]==node) return node;
-        return parent[node]=findHighestParent(parent[node]);
+    int ultimateparent(int node){
+        if(node==parent[node]) return node;
+        return parent[node]=ultimateparent(parent[node]);
     }
-    bool find(int u, int v) {
-        return (findHighestParent(u)==findHighestParent(v));
-    }
-
-    void unionByRank(int u, int v) {
-        int parentofU=findHighestParent(u);
-        int parentofV=findHighestParent(v);
-        if(parentofU==parentofV) return;
-        if(rank[parentofU]>rank[parentofV]){
-            parent[parentofV]= parentofU;
-        }
-        else if(rank[parentofU]<rank[parentofV]){
-            parent[parentofU]= parentofV;
-        }
-        else{
-            parent[parentofU]= parentofV;
-            rank[parentofV]++;
-        }
+    bool find(int u,int v){
+        return (ultimateparent(u) == ultimateparent(v));
     }
 
-    void unionBySize(int u, int v) {
-        int parentofU=findHighestParent(u);
-        int parentofV=findHighestParent(v);
-        if(parentofU==parentofV) return;
-        if(size[parentofU]>size[parentofV]){
-            parent[parentofV]= parentofU;
-            size[parentofU]+=size[parentofV];
+    void unionbyrank(int u,int v){
+        int parent_u=ultimateparent(u);
+        int parent_v=ultimateparent(v);
+        if(parent_u==parent_v) return;
+        if(rank[parent_u]>rank[parent_v]){
+            parent[parent_v]=parent_u;
+        }
+        else if(rank[parent_u]<rank[parent_v]){
+            parent[parent_u]=parent_v;
         }
         else{
-            parent[parentofU]= parentofV;
-            size[parentofV]+=size[parentofU];
+            parent[parent_u]=parent_v;
+            rank[parent_v]++;
+        }
+
+    }
+    void unionbysize(int u,int v){ //mistake
+        int parent_u=ultimateparent(u);
+        int parent_v=ultimateparent(v);
+        if(parent_u==parent_v) return;
+        if(size[parent_u]>size[parent_v]){
+            parent[parent_v]=parent_u;
+            size[parent_u]+=size[parent_v];
+        }
+        else{
+            parent[parent_u]=parent_v;
+            size[parent_v]+=size[parent_u];
         }
     }
-};
+}; //mistake
 
 
 class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
-        int totalEdges=connections.size();
-        if(totalEdges<n-1) return -1;
-        DisjointSet ds(n);
-        for(int i=0;i<totalEdges;i++){
-            ds.unionByRank(connections[i][0],connections[i][1]);
+        int NoOfEdges=connections.size();
+        if(NoOfEdges<n-1) return -1;
+        disjoinSet ds(n);
+        for(auto i : connections ){
+            ds.unionbyrank(i[0],i[1]);
         }
-
-        int count=0; //total connected component
+        //cnt total  number of component
+        int cnt=0;
         for(int i=0;i<n;i++){
-            if(ds.parent[i]==i) count++;
+            if(ds.parent[i]==i) cnt++; //mistake
         }
 
-        return count-1; //requirededges=connected componenet -1;
+        return cnt-1;
 
     }
 };
