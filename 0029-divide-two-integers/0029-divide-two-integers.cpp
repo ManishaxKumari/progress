c383e84381961001ac32ee983da3a840b0a49df8
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-06-18 06:37:35 +0530

    Time: 0 ms (100%), Space: 8.5 MB (71.49%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        //edges case
-        if(dividend==divisor) return 1;
-        if(dividend==INT_MIN && divisor == -1 ) return INT_MAX;
-        if(divisor==1) return dividend;
+        
+        // Edge cases
+        if(dividend == divisor) return 1;
+        if(dividend == INT_MIN && divisor == -1) return INT_MAX;
+        if(divisor == 1) return dividend;
+        bool isPositive =!((dividend >= 0 && divisor < 0) ||(dividend <= 0 && divisor > 0));
+        long n = dividend;
+        long d = divisor;
+        
+        n = abs(n);
+        d = abs(d);
+        int ans = 0;
 
-        bool ispositive=((dividend>0 && divisor>0) || (dividend<0 && divisor<0));
-        long long n=abs((long long)dividend);
-        long long d=abs((long long)divisor);
-
-        long long ans=0;
-        while(n>=d){
-            n-=d;
-            ans++;
+        while(n >= d) {
+            int count = 0;
+            while(n >= (d << (count+1))) {
+                count++;
+            }
+            
+            // Updating the answer & dividend
+            ans += (1 << count);
+            n -= (d << count);
         }
-        if(ans>INT_MAX){
-            return ispositive? INT_MAX : INT_MIN;
-        }
-        return ispositive?ans: -ans;
-
+        
+        // Handling overflowing condition
+        if(ans == (1 << 31) && isPositive) 
+            return INT_MAX;
+        if(ans == (1 << 31) && !isPositive)
+            return INT_MIN;
+        
+        /* Returning the quotient 
+        with proper sign */
+        return isPositive ? ans : -1*ans;
     }
 };
