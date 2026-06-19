7de967d62d68f0e2737b258e67b91be2a7ca894c
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-06-19 16:46:56 +0530

    Time: 0 ms (100%), Space: 21.6 MB (75.28%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>>ans;
+        int n=intervals.size();
         int i=0;
-        while(i<intervals.size()){
-            if(intervals[i][1]<newInterval[0]) i++;
+        while(i<n){
+            if(intervals[i][1]<newInterval[0]){
+                ans.push_back(intervals[i]);
+            }
             else if(intervals[i][0]>newInterval[1]){
-                intervals.insert(intervals.begin()+i,newInterval);
-                return intervals;
+                break;
             }
             else{
-                //overlap , merge and then erase
                 newInterval[0]=min(newInterval[0],intervals[i][0]);
                 newInterval[1]=max(newInterval[1],intervals[i][1]);
-                intervals.erase(intervals.begin()+i);
             }
+            i++;
         }
-        intervals.push_back(newInterval);
-        return intervals;
+        ans.push_back(newInterval);
+        while(i<n){
+            ans.push_back(intervals[i]);
+            i++;
+        }
+        return ans;
     }
 };
