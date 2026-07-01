fa636e3a2c5e8301f53e381feedeed6b2c6d8ac0
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-07-01 22:46:07 +0530

    Time: 0 ms (100%), Space: 8.9 MB (42.12%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    void solve(int k , int n , vector<vector<int>> &result,vector<int> &path,int index){
-        //store valid path
+    vector<vector<int>> ans;
+    vector<int>v;
+    void solve(int k,int n,int idx){
         if(k==0 && n==0){
-            result.push_back(path);
+            ans.push_back(v);
+            return;
         }
-        // remove invalid path
-        if(k==0 || n<0) return;
-
-        for(int i=index; i<=9;i++){
-            path.push_back(i);
-            solve(k-1,n-i,result,path,i+1);
-            path.pop_back();
+        if(n<0 || k<0) return;
+        for(int i=idx;i<=9;i++){
+            v.push_back(i);
+            solve(k-1,n-i,i+1);
+            v.pop_back();
         }
-
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>>result;
-        vector<int>path;
-        solve(k,n,result,path,1);
-        return result;
+        solve(k,n,1);
+        return ans;
     }
 };
