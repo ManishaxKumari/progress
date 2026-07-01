b4066e42258b0726300a5b80b21eef1096f48202
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-07-01 11:34:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     double func(double x, int n){
         if(n<=0) return 1.0;
+        if(n%2==0) return func(x*x,n/2);
         return x*func(x,n-1);
     }
     double myPow(double x, int n) {
