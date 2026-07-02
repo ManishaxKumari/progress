07e46bf232b961811d76f98163282214621426fe
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-07-02 19:46:23 +0530

    Time: 11 ms (13.42%), Space: 12 MB (20.67%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,47 +1,37 @@
 class Solution {
 public:
-    bool issafe(vector<string>& board,int row,int col){
-        int r=row;
-        int c=col;
-        //check for top
-        while(r>=0){
-            if(board[r][c]=='Q') return false;
-            r--;
-        }
-        r=row;
-        c=col;
-        //check for upper left diagonal
-        while(r>=0 && c>=0){
-            if(board[r][c]=='Q') return false;
-            r--;
-            c--;
-        }
-        r=row;
-        c=col;
-        //check for upper right diagonal
-        while(r>=0 && c>=0){
-            if(board[r][c]=='Q') return false;
-            r--;
-            c++;
-        }
-        return true;
-    }
+    unordered_set<int>cols;
+    unordered_set<int>Ldia;
+    unordered_set<int>Rdia;
     void solve(int row,vector<vector<string>> &ans, vector<string>& board){
         if(row==board.size()){
             ans.push_back(board);
             return;
         }
         for(int col=0;col<board[0].size();col++){
-            if(issafe(board,row,col)){
-                board[row][col]='Q';
-                solve(row+1,ans,board);
-                board[row][col]='.';
-            }
+            int LeftDiaConst= row+col;
+            int rightDiaConst=row-col;
+
+            if(cols.find(col)!=cols.end() || Ldia.find(LeftDiaConst)!=Ldia.end()||Rdia.find(rightDiaConst)!=Rdia.end()) continue;
+
+            cols.insert(col);
+            Ldia.insert(LeftDiaConst);
+            Rdia.insert(rightDiaConst);
+            board[row][col]='Q';
+
+            solve(row+1,ans,board);
+
+            board[row][col]='.';
+            cols.erase(col);
+            Ldia.erase(LeftDiaConst);
+            Rdia.erase(rightDiaConst);           
+
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>>ans;
         vector<string>board(n,string (n,'.'));
+
         solve(0,ans,board);
         return ans;
 
