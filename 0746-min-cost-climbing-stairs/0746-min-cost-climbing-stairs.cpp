072c47fbf0361a1a13a4fb63e781a95ee920fbb7
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-07-03 22:14:14 +0530

    Time: 3 ms (8.51%), Space: 17.7 MB (51.05%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,19 +1,14 @@
 class Solution {
-public:
-//space optimization
+public: 
+    int solve(vector<int>& cost,int n,vector<int> &dp){
+        if(n<=1) return cost[n];
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=cost[n]+min(solve(cost,n-1,dp),solve(cost,n-2,dp));
+    }
     int minCostClimbingStairs(vector<int>& cost) {
-        cost.push_back(0); // because their is no cost to reach top
+        cost.push_back(0);
         int n=cost.size();
-        vector<int> dp(n);
-        //base case
-        int prev2=cost[0];
-        int prev1=cost[1];
-        for(int i=2;i<n;i++){
-            int curr=cost[i]+min(prev1,prev2);
-            prev2=prev1;
-            prev1=curr;
-        }
-
-        return prev1;
+        vector<int>dp(n+1,-1);
+        return solve(cost,n-1,dp);
     }
 };
