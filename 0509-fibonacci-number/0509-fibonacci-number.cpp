5fd3e3408fcc530174320ab750063938d3e38bb2
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-07-03 21:25:43 +0530

    Time: 0 ms (100%), Space: 8.3 MB (10.66%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,8 +1,23 @@
 class Solution {
 public:
-    //recursion
-    int fib(int n) {
+    //---------recursion------------------
+
+    // int fib(int n) {
+    //     if(n<=1) return n;
+    //     return fib(n-1)+fib(n-2);
+    // }
+
+    //----------------------------------memorization--------------------------
+    int solve(int n,vector<int> &dp){
         if(n<=1) return n;
-        return fib(n-1)+fib(n-2);
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
     }
+    int fib(int n){
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
+    }
+
+
+
 };
