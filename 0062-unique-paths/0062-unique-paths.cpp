c9a518c290f9142ca4c39f1d3dbde372e3a00258
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-07-04 13:47:41 +0530

    Time: 0 ms (100%), Space: 9.6 MB (5.06%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-//memorization
+//tabulation
     int solve(int i,int j,vector<vector<int>> &dp){
         if(i==0 && j==0 ) return 1;
         if(i<0 || j<0) return 0;
@@ -10,7 +10,24 @@ public:
         return dp[i][j]=left+up;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solve(m-1,n-1,dp);
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        for(int i=0;i<n;i++){
+            dp[0][i]=1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j=0;j<n;j++){
+                int up=0;
+                int left=0;
+                if(i-1>=0){
+                    up=dp[i-1][j];
+                }
+                if(j-1>=0){
+                    left=dp[i][j-1];
+                }
+                dp[i][j]=up+left;
+            }
+                
+        }
+        return dp[m-1][n-1];
     }
 };
