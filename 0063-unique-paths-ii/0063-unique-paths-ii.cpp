70eacfb4a7bbe3a7c444c7cb7c4d0d57719c0cc7
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-07-04 14:37:45 +0530

    Time: 0 ms (100%), Space: 11.5 MB (85.21%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
-//tabulation
+//space optimization
     int uniquePathsWithObstacles(vector<vector<int>>& Grid) {
         int n=Grid.size();
         int m=Grid[0].size();
-        vector<vector<int>>dp(n,vector<int>(m));
+        vector<int>prev(m,0);
         for(int i=0;i<n;i++){
+            vector<int>temp(m,0);
             for(int j=0;j<m;j++){
                 if(Grid[i][j]==1){
-                    dp[i][j]=0;
+                    temp[j]=0;
                     continue;
                 } 
                 if(i==0 && j==0){
-                    dp[i][j]=1;
+                    temp[j]=1;
                     continue;
                 }
 
                 int up=0;
                 int left=0;
                 if(i>0){
-                    up=dp[i-1][j];
+                    up=prev[j];
                 }
                 if(j>0){
-                    left=dp[i][j-1];
+                    left=temp[j-1];
                 }
-                dp[i][j]=left+up;
+                temp[j]=left+up;
 
             }
+            prev=temp;
         }
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
 };
 
