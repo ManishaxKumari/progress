f8b8dbc8e71600a2dad14108b93a2511503bed18
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-07-05 09:00:45 +0530

    Time: 0 ms (100%), Space: 9.4 MB (53.49%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-//space optimization
-    int uniquePaths(int m, int n) {
-        vector<int>prev(n,0);
-        for(int i=0;i<m;i++){
-            vector<int>temp(n,0);
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0) {
-                    temp[j]=1;
-                    continue;
-                }
-                int up=0;
-                int left=0;
-                if(i-1>=0){
-                    up=prev[j];
-                }
-                if(j-1>=0){
-                    left=temp[j-1];
-                }
-                temp[j]=up+left;
-            }
-            prev=temp;
+    //memo
+    int solve(int i,int j,vector<vector<int>> &dp){
+        //base case-> reach destination / out of bound
+        if(i==0 && j==0) { //reach destination
+            return 1;
         }
-        return prev[n-1];
+        if(i<0 || j<0) return 0; // out of bound
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up=solve(i-1,j,dp);
+        int left=solve(i,j-1,dp);
+        return dp[i][j]=up+left;
+
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return solve(m-1,n-1,dp);
     }
 };
