7a96cecebb11e7d085cd086ca10f7b6be1cbadd2
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-07-07 12:21:52 +0530

    Time: 107 ms (70.05%), Space: 109.6 MB (46.21%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    //tabulation
+    //space optimization
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int sum=accumulate(nums.begin(),nums.end(),0);
         if((sum % 2)!=0) return false;
         int target=(sum/2);
-        vector<vector<int>>dp(n,vector<int>(target+1,0));
-        for(int i=0;i<n;i++){
-            dp[i][0]=1;
-        }
+        vector<int>prev(target+1,0);
+        prev[0]=1;
         if(nums[0]<=target){
-            dp[0][nums[0]]=1;
+            prev[nums[0]]=1;
         }
         for(int i=1;i<n;i++){
+            vector<int>temp(target+1,0);
+            temp[0]=1;
             for(int j=1;j<=target;j++){
-                int notake=dp[i-1][j];
+                int notake=prev[j];
                 int take=0;
                 if(nums[i]<=j){
-                    take=dp[i-1][j-nums[i]];
+                    take=prev[j-nums[i]];
                 }
-                dp[i][j]=take || notake;
+                temp[j]=take || notake;
             }
+            prev=temp;
         }
-        return dp[n-1][target];
+        return prev[target];
     }
 };
