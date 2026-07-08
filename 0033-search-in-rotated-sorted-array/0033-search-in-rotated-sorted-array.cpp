ccf8f4885b1008947067f2abd71572f8df811a8d
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-07-08 16:45:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -5,23 +5,25 @@ public:
         int low=0;
         int high=n-1;
         while(low<=high){
-            int mid=(low+high)/2;
+            int mid=low+(high-low)/2;
             if(nums[mid]==target) return mid;
-            //check which part is sorted
-            if(nums[mid]<nums[high]){ // right sorted
-                if(nums[mid]<=target && target<=nums[high]){ // present in sorted part;
-                    low=mid+1;
-                }
-                else high=mid-1;
-            }
-            else{
-                if(nums[low]<=target && target<=nums[mid]){
+            if(nums[low]<nums[mid]){ // left part sorted
+                if(nums[low]<=target && target<=nums[mid]){ // exist in sorted part
                     high=mid-1;
                 }
-                else low=mid+1;
+                else{ //exist in unsorted part
+                    low=mid+1;
+                }
+            }
+            else{//right part sorted
+                if(nums[mid]<=target && target<=nums[high]){ //exist in sorted part
+                    low=mid+1;
+                }
+                else{ //exist in unsorted part
+                    high=mid-1;
+                }
             }
         }
         return -1;
-
     }
 };
