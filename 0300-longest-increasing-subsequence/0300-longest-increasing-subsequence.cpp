60cf397abfeb9576e66761c6c27a14c10ab2db32
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-07-08 09:14:36 +0530

    Time: 79 ms (46.36%), Space: 14.5 MB (48.12%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    //tabulation
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,1);
+        int maxlis=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+              if(nums[j]<nums[i]){
+                dp[i]=max(dp[i],dp[j]+1);
+                maxlis=max(maxlis,dp[i]);
+              } 
+            }
+        }
+        return maxlis;
+    }
+};
