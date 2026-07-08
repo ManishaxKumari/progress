d16ecdd5ff96a9d2027e310aaa87abb747183e04
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-07-08 12:44:10 +0530

    Time: 0 ms (100%), Space: 13.7 MB (43.1%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -5,12 +5,12 @@ public:
         int low=0;
         int high=n-1;
         while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]>=target){
-                high=mid-1;
-            }
-            else low=mid+1;
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target) low=mid+1;
+            else high=mid-1;
         }
         return low;
+
     }
 };
