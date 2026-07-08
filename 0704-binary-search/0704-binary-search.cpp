1fb24e99f1370d4b941efb82c094b22b006677de
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-07-08 12:36:02 +0530

    Time: 0 ms (100%), Space: 31.3 MB (46.13%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -5,10 +5,14 @@ public:
         int low=0;
         int high=n-1;
         while(low<=high){
-            int mid=(low+high)/2;  // low+(high-low)/2
-            if(nums[mid]==target) return mid;
-            else if(nums[mid]>target) high=mid-1;
-            else low=mid+1;
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+            else high=mid-1;
         }
         return -1;
     }
