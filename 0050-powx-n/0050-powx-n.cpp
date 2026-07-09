1baf4f3e0541b0c45c9229b313cd929406959a73
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-07-09 15:28:32 +0530

    Time: 0 ms (100%), Space: 8.7 MB (12.02%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,16 +1,22 @@
 class Solution {
-using ll=long long;
 public:
-    double func(double x, ll n){
-        if(n<=0) return 1.0; //base case
-        //choice -> odd/even
-        if(n%2==0) return func(x*x,n/2);
-        return x*func(x,n-1);
+    double solve(double x, long long n){
+        if(n==0) return 1.0;
+        if(n%2==0){ //even
+            return solve(x*x,n/2);
+        }
+        else {
+            return x* solve(x,n-1);
+        }
     }
-    double myPow(double x, int num) {
-        ll n=num;
-        //choice n-> positive or negative
-        if(n<0) return (1.0/func(x,-1*n));
-        return func(x,n);
+    double myPow(double x, int n) {
+        long long num=n;
+        if(num<0){
+            return solve(1/x,-1*num);
+        }
+        else{
+            return solve(x,num);
+        }
+        
     }
 };
