750cd59285982f8189bbb450e949fb2baf8a6d6e
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-07-10 19:45:01 +0530

    Time: 15 ms (92.17%), Space: 9.1 MB (96.92%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-//tabulation
+//space optimization
     int longestCommonSubsequence(string s, string t) {
         int n=s.size();
         int m=t.size();
-
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0)); // because dp[i][j]= till i lenghth and j lenght what is my lcs !! so for n and m length  i need n+1,m+1 matrix;
-        // base case ki jarrurt nhi h kyuko 0 se already feel h dp[i][0] and dp[0][j] agar ak string ka len bhi 0 hua to koi matching nhi hogi
-        for(int i=1;i<=n;i++){
+        vector<int> prev(m+1,0);
+        vector<int>curr(m+1,0);
+        for(int i=1;i<=n;i++){ 
             for(int j=1;j<=m;j++){
-                if(s[i-1]==t[j-1]){ // becuase length 1 means - index 0 and leng 2 means index 1 so min 1
-                    dp[i][j]=1+dp[i-1][j-1];
+                if(s[i-1]==t[j-1]){
+                    curr[j]=1+prev[j-1];
                 }
                 else {
-                    dp[i][j]=max(dp[i-1][j] , dp[i][j-1]);
+                    curr[j]=max(prev[j] ,curr[j-1]);
                 }
             }
+            prev=curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
