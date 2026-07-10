785bebb21833f6dd70a3d94e3c99bf2d6c3b7d2c
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-07-10 19:36:22 +0530

    Time: 19 ms (86.76%), Space: 27.5 MB (53.72%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int solve(int i,int j,string &s,string &t, vector<vector<int>> &dp){ //state
-        //base case
-        if(i<0 || j<0 ) return 0;
-        //match
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j]){
-            return dp[i][j]=1 + solve(i-1,j-1,s,t,dp);
+//tabulation
+    int longestCommonSubsequence(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0)); // because dp[i][j]= till i lenghth and j lenght what is my lcs !! so for n and m length  i need n+1,m+1 matrix;
+        // base case ki jarrurt nhi h kyuko 0 se already feel h dp[i][0] and dp[0][j] agar ak string ka len bhi 0 hua to koi matching nhi hogi
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){ // becuase length 1 means - index 0 and leng 2 means index 1 so min 1
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else {
+                    dp[i][j]=max(dp[i-1][j] , dp[i][j-1]);
+                }
+            }
         }
-        //not match
-        return dp[i][j]=max(solve(i-1,j,s,t,dp),solve(i,j-1,s,t,dp));
-    }
-    int longestCommonSubsequence(string text1, string text2) {
-        int n=text1.size();
-        int m=text2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,text1,text2,dp);
+        return dp[n][m];
     }
 };
