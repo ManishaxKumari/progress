8d0909f8735383a2511eb023cdf79ddf374c0c0e
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-07-11 17:45:13 +0530

    Time: 32 ms (28.08%), Space: 11 MB (46.8%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    bool ispalindrome(string &s ,int l,int r,vector<vector<int>> &dp){
-        if(l>r) return true;
-        if(dp[l][r]!=-1) return dp[l][r];
-        if(s[l]==s[r]){
-            return dp[l][r]=ispalindrome(s,l+1,r-1,dp);
-        }
-        return dp[l][r]= false;
-    }
+    //tabulation
     int countSubstrings(string s) {
         int n=s.size();
+        vector<vector<bool>> dp(n,vector<bool>(n,false));
         int cnt=0;
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(ispalindrome(s,i,j,dp)){
-                    cnt++;
+        for(int len=1;len<=n;len++){
+            for(int i=0;i+len-1<n;i++){
+                int j=i+len-1;
+
+                if(i==j) dp[i][j]=true;
+                else if(i+1 == j){
+                    dp[i][j]=(s[i]==s[j]);
                 }
+                else{
+                    dp[i][j]=(s[i]==s[j] && dp[i+1][j-1]);
+                }
+                if(dp[i][j]==true) cnt++;
+
             }
         }
         return cnt;
+
+
     }
 };
