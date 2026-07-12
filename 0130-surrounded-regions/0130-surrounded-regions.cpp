f870463d3b6698f4b6cc392db3a0a7442566e54b
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-07-12 15:07:55 +0530

    Time: 3 ms (22.65%), Space: 14.4 MB (70.99%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -2,58 +2,31 @@ class Solution {
 public:
     int dr[4]={-1,0,1,0};
     int dc[4]={0,1,0,-1};
-
-    bool valid(int i,int j,int m,int n){
+    bool isvalid(int i,int j,int m,int n){
         return i>=0 && j>=0 && i<m && j<n;
     }
-
+    void dfs(int r,int c,vector<vector<char>>& board,vector<vector<int>> &vis,int m,int n){
+        vis[r][c]=1;
+        for(int i=0;i<4;i++){
+            int nr=r+dr[i];
+            int nc=c+dc[i];
+            if(isvalid(nr,nc,m,n) && !vis[nr][nc] && board[nr][nc]=='O'){
+                dfs(nr,nc,board,vis,m,n);
+            }
+        }
+    }
     void solve(vector<vector<char>>& board) {
         int m=board.size();
         int n=board[0].size();
-
-        vector<vector<int>> vis(m,vector<int>(n,0));
-        queue<pair<int,int>> q;
-
-        // boundary
+        vector<vector<int>>vis(m,vector<int>(n,0));
         for(int i=0;i<m;i++){
-            if(board[i][0]=='O'){
-                q.push({i,0});
-                vis[i][0]=1;
-            }
-            if(board[i][n-1]=='O'){
-                q.push({i,n-1});
-                vis[i][n-1]=1;
-            }
+            if(board[i][0]=='O') dfs(i,0,board,vis,m,n);
+            if(board[i][n-1]=='O')  dfs(i,n-1,board,vis,m,n);
         }
-
         for(int j=0;j<n;j++){
-            if(board[0][j]=='O'){
-                q.push({0,j});
-                vis[0][j]=1;
-            }
-            if(board[m-1][j]=='O'){
-                q.push({m-1,j});
-                vis[m-1][j]=1;
-            }
+            if(board[0][j]=='O') dfs(0,j,board,vis,m,n);
+            if(board[m-1][j]=='O')  dfs(m-1,j,board,vis,m,n);
         }
-
-        // BFS
-        while(!q.empty()) {
-            auto [r, c] = q.front();
-            q.pop();
-
-            for(int i = 0; i < 4; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-
-                if(valid(nr, nc, m, n) && board[nr][nc]=='O' && vis[nr][nc]==0){
-                    vis[nr][nc]=1;
-                    q.push({nr,nc});
-                }
-            }
-        }
-
-        // convert
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(board[i][j]=='O' && vis[i][j]==0){
@@ -61,5 +34,6 @@ public:
                 }
             }
         }
+
     }
 };
