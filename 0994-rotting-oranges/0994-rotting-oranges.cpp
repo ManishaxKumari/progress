d3be65cd4b11e8ea75a793161466e1cc36f9cd51
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-07-12 14:03:12 +0530

    Time: 0 ms (100%), Space: 16.7 MB (86.55%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
     int dr[4]={-1,0,1,0};
     int dc[4]={0,1,0,-1};
-    bool valid(int i,int j,int m,int n){
+    bool isvalid(int i,int j,int m,int n){
         return i>=0 && j>=0 && i<m && j<n;
     }
 
     int orangesRotting(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-
-        vector<vector<int>>vis(m,vector<int>(n,0));
         int freshOrg=0;
-        queue<pair<int,int>>q;
+        queue<pair<int,int>> q;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
-                    vis[i][j]=1;
                     q.push({i,j});
                 }
                 else if(grid[i][j]==1){
@@ -26,7 +23,7 @@ public:
         }
 
         int time=0;
-        while(!q.empty() && freshOrg>0){
+        while(!q.empty() && freshOrg>0 ){
             int size=q.size();
             for(int i=0;i<size;i++){
                 auto [r,c]=q.front();
@@ -34,16 +31,17 @@ public:
                 for(int i=0;i<4;i++){
                     int nr=r+dr[i];
                     int nc=c+dc[i];
-                    if(valid(nr,nc,m,n) && !vis[nr][nc] && grid[nr][nc]==1){
-                        q.push({nr,nc});
-                        vis[nr][nc]=1;
+                    if(isvalid(nr,nc,m,n) && grid[nr][nc]==1){
+                        grid[nr][nc]=2;
                         freshOrg--;
+                        q.push({nr,nc});
                     }
                 }
             }
             time++;
         }
-        return freshOrg==0? time:-1;
+
+        return freshOrg==0 ?  time : -1;
 
     }
 };
