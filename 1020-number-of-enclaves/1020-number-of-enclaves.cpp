05e7ad652fdf9cb69086292a627403c74587af2d
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-07-12 11:39:41 +0530

    Time: 5 ms (82.58%), Space: 33.3 MB (58.36%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,48 +1,41 @@
 class Solution {
 public:
-
     int dr[4]={-1,0,1,0};
     int dc[4]={0,1,0,-1};
-    bool valid(int i,int j,int m,int n){
-        return i>=0 && j>=0 && i<m && j<n ;
+    bool isvalid(int i,int j , int m,int n){
+        return i>=0 && j>=0 && i<m && j<n;
     }
 
-    void dfs(vector<vector<int>> &ans,int r,int c){
-        ans[r][c]=0; //visited
-        int m=ans.size();
-        int n=ans[0].size();
+    void dfs( int r,int c,vector<vector<int>>& grid ,int m,int n){
+        grid[r][c]=0;
         for(int i=0;i<4;i++){
             int nr=r+dr[i];
             int nc=c+dc[i];
-            if(valid(nr,nc,m,n) && ans[nr][nc]==1){
-                dfs(ans,nr,nc);
+            if(isvalid(nr,nc,m,n)&& grid[nr][nc]==1){
+                dfs(nr,nc,grid,m,n);
             }
         }
     }
-
     int numEnclaves(vector<vector<int>>& grid) {
-        int m=grid.size(); 
+        int m=grid.size();
         int n=grid[0].size();
-        vector<vector<int>> ans=grid;
         for(int i=0;i<m;i++){
-            if(ans[i][0]==1) dfs(ans,i,0);
-            if(ans[i][n-1]==1) dfs(ans,i,n-1);
+            if(grid[i][0]==1) dfs(i,0,grid,m,n);
+            if(grid[i][n-1]==1) dfs(i,n-1,grid,m,n);
         }
         for(int j=0;j<n;j++){
-            if(ans[0][j]==1) dfs(ans,0,j);
-            if(ans[m-1][j]==1) dfs(ans,m-1,j);
+            if(grid[0][j]==1) dfs(0,j,grid,m,n);
+            if(grid[m-1][j]==1) dfs(m-1,j,grid,m,n);
         }
         int cnt=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(ans[i][j]==1){
+                if(grid[i][j]==1){
                     cnt++;
                 }
             }
         }
         return cnt;
-
-
 
     }
 };
