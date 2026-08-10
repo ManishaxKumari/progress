4ecdb6fe25afab8652b47fa1c86858c0757cfb09
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-08-10 07:30:07 +0530

    Time: 0 ms (100%), Space: 12.8 MB (48.53%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -15,7 +15,8 @@ public:
         if(p==nullptr && q==nullptr) return true;
         if(p==nullptr || q==nullptr) return false;
         if(p->val !=q->val) return false;
-        return  isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
-
+        bool left=isSameTree(p->left,q->left);
+        bool right=isSameTree(p->right,q->right);
+        return left && right;
     }
 };
