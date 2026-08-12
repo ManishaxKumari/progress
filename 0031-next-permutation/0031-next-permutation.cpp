291acb09bcc432abd8ffc0cc0e3bb16c8c46caba
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-08-12 10:59:28 +0530

    Time: 0 ms (100%), Space: 15.8 MB (51.39%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -3,7 +3,6 @@ public:
     void nextPermutation(vector<int>& nums) {
         int n=nums.size();
         int index=-1;
-        // find break point
         for(int i=n-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
                 index=i;
@@ -14,6 +13,7 @@ public:
             reverse(nums.begin(),nums.end());
             return;
         }
+
         for(int i=n-1;i>=index;i--){
             if(nums[i]>nums[index]){
                 swap(nums[i],nums[index]);
