b14009203a16d6411d32f768df7bcfd4fc6c1a0f
Manisha <140229411+ManishaxKumari@users.noreply.github.com>
2026-08-12 11:14:13 +0530

    Time: 43 ms (83.58%), Space: 29.2 MB (29.18%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,8 +4,8 @@ public:
         int n=nums.size();
         vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<n-1;i++){
-            //condition skipping the duplicateds 
+
+        for(int i=0;i<n;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
             int j=i+1;
             int k=n-1;
@@ -18,16 +18,15 @@ public:
                     k--;
                 }
                 else{
-                    vector<int> temp={nums[i],nums[j],nums[k]};
-                    ans.push_back(temp);
+                    ans.push_back({nums[i],nums[j],nums[k]});
                     j++;
                     k--;
                     while(j<k && nums[j]==nums[j-1]) j++;
-                    while(j<k && nums[k]== nums[k+1]) k--;
+                    while(j<k && nums[k]==nums[k+1]) k--;
+
                 }
             }
         }
         return ans;
-
     }
 };
